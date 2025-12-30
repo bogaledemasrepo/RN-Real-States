0170b6677ee0234825bec7f30cd7001cf186c492
bogidemasrepo <bogidemas@gmail.com>
2025-12-30 23:31:10 +0300

    tab bar update implement icon & label change

--- app/(root)/tabs/_layout.tsx
+++ app/(root)/tabs/_layout.tsx
@@ -1,12 +1,13 @@
+import { FontAwesome5, MaterialIcons } from '@expo/vector-icons'
 import { Tabs } from 'expo-router'
 import React from 'react'
 
 const TabsLayout = () => {
   return (
     <Tabs screenOptions={{headerShown:false}}>
-      <Tabs.Screen name='index' />
-      <Tabs.Screen name='explore' />
-      <Tabs.Screen name='profile' />
+    <Tabs.Screen name="index" options={{title:"Home",tabBarLabel:"Home",tabBarIcon:({color,focused,size})=><FontAwesome5 name="home" size={size} color={color} />}} />
+    <Tabs.Screen name="explore" options={{title:"Explore",tabBarLabel:"Explore",tabBarIcon:({color,focused,size})=><MaterialIcons name="explore" size={size} color={color} />}} />
+    <Tabs.Screen name="profile" options={{title:"Profile",tabBarLabel:"Profile",tabBarIcon:({color,focused,size})=><FontAwesome5 name="user" color={color} size={size}/>}}/>
     </Tabs>
   )
 }
