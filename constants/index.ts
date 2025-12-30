8ef3355df270ad412317d1c9be9e3ad0f9625026
bogidemasrepo <bogidemas@gmail.com>
2025-12-30 22:40:23 +0300

    add dummy data

---
+++ constants/index.ts
@@ -0,0 +1,20 @@
+import { Property } from "@/types";
+
+export const RECOMMENDATIONS: Property[] = [
+  {
+    id: "1",
+    name: "La Grand Maison",
+    location: "Tokyo, Japan",
+    price: "$12219",
+    rating: "4.8",
+    imageUrl: "https://images.unsplash.com/photo-1512917774080-9991f1c4c750",
+  },
+  {
+    id: "2",
+    name: "Modernica App",
+    location: "Tokyo, Japan",
+    price: "$1424",
+    rating: "4.8",
+    imageUrl: "https://images.unsplash.com/photo-1600585154340-be6161a56a0c",
+  },
+];
